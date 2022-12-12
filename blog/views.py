82e0903c3e5b95cb0683936c18bb3b2dbffd9d41
Joe Britton <joebritton@outlook.com>
2022-12-12 15:39:49 +0000

    added paginated view and SEO friendly URls to the blog

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,28 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 from .models import Post
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 def post_list(request):
-    posts = Post.published.all()
+    post_list = Post.published.all()
+    # pagination with 3 posts per page
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        # if page number is not an integer, deliver the first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # if page number is out of range deliver last page of results
+        posts = paginator.page(paginator.num_pages)   
     return render(request, 'blog/post/list.html', {'posts': posts})
     
-def post_detail(request, id):
-    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post, status=Post.Status.PUBLISHED,
+                             slug=post,
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
     return render(request, 'blog/post/detail.html', {'post': post})
