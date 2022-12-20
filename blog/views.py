5c30c9592a093f5019bfc04a2554662fece41564
Joe Britton <joebritton@outlook.com>
2022-12-20 14:55:10 +0000

    added tags to the Post model using django-taggit, also implemented custom template tags for the front end

--- blog/views.py
+++ blog/views.py
@@ -7,10 +7,16 @@ from django.core.mail import send_mail
 from django.views.generic import ListView
 from django.views.decorators.http import require_POST
 from .forms import EmailPostForm, CommentForm
+from taggit.models import Tag
+from django.db.models import Count
 
 # Create your views here.
-def post_list(request):
+def post_list(request, tag_slug=None):
     post_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags__in=[tag])
     # pagination with 3 posts per page
     paginator = Paginator(post_list, 3)
     page_number = request.GET.get('page', 1)
@@ -22,7 +28,8 @@ def post_list(request):
     except EmptyPage:
         # if page number is out of range deliver last page of results
         posts = paginator.page(paginator.num_pages)   
-    return render(request, 'blog/post/list.html', {'posts': posts})
+    return render(request, 'blog/post/list.html', {'posts': posts,
+                                                   'tag': tag})
     
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, status=Post.Status.PUBLISHED,
@@ -34,9 +41,16 @@ def post_detail(request, year, month, day, post):
     comments = post.comments.filter(active=True)
     # form for users to comment
     form = CommentForm()
+    # list of similar posts
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids)\
+                                    .exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags'))\
+                                    .order_by('-same_tags','-publish')[:4]
     return render(request, 'blog/post/detail.html', 
                   {'post': post, 'comments': comments,
-                   'form': form})
+                   'form': form,
+                   'similar_posts': similar_posts})
 
 class PostListView(ListView):
     """
--- mysite/settings.py
+++ mysite/settings.py
@@ -33,6 +33,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
+    'taggit',
 ]
 
 MIDDLEWARE = [
